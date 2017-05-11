ed16dea5c889ab02e05078389b46e5e9bd25693b
Henry Thomas <Henry Thomas>
2017-05-11 11:03:20 +0100

    completed outliers section. Using adapted (and extreme) version of "outer fence". Only "TOTAL" being removed.

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -7,6 +7,10 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
@@ -17,6 +21,62 @@ with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
 ### Task 2: Remove outliers
+def graph_feature_distribution(data_dict, feature):
+	feature_data = []
+	for elem in data_dict:
+		feature_data.append(data_dict[elem][feature])
+
+	feature_numpy = np.asarray(feature_data).astype(float)
+	feat_num_cl = feature_numpy[~np.isnan(feature_numpy)]
+	maxi = max(feat_num_cl)
+	mini = min(feat_num_cl)
+	binwidth = (maxi-mini)/100
+	plt.hist(feat_num_cl, bins = np.arange(min(feat_num_cl), max(feat_num_cl) + binwidth, binwidth))
+	plt.xlabel(feature)
+	plt.ylabel('count')
+	plt.show()
+
+def find_fence(data_dict, feature):
+	feature_data = []
+	for elem in data_dict:
+		feature_data.append(data_dict[elem][feature])
+
+	feature_numpy = np.asarray(feature_data).astype(float)
+	feat_num_cl = feature_numpy[~np.isnan(feature_numpy)]
+	q3, q1 = np.percentile(feat_num_cl, [75, 25])
+	outer_max = 10 * (q3 - q1) + q3
+	outer_min = q1 - 10 * (q3 - q1) 
+
+	return outer_max, outer_min
+	
+
+upper, lower = find_fence(data_dict, 'salary')
+
+def remove_feature_outliers(data_dict, feature):
+	upper, lower = find_fence(data_dict, feature)
+	data_dict_clean = {}
+
+	for elem in data_dict:
+		value = data_dict[elem][feature]
+		if not (value != 'NaN' and (value > upper or value < lower)):
+			data_dict_clean[elem] = data_dict[elem]
+
+	return data_dict_clean
+
+
+
+def remove_all_outliers(data_dict, features_list):
+	for feature in features_list:
+		data_dict_clean = remove_feature_outliers(data_dict, feature)
+	return data_dict_clean
+
+
+
+data_dict = remove_all_outliers(data_dict, features_list)
+
+
+
+
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
